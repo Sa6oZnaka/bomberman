c126296803b11a6630168e46d40f5b6de6d8ab21
Alexadnar Karapenev <treildr@gmail.com>
2019-11-03 15:24:17 +0200

    Added random barricade generation

--- src/api/GameMap.js
+++ src/api/GameMap.js
@@ -12,15 +12,18 @@ export class GameMap {
         for (let i = 0; i < x; ++i) {
             map[i] = [];
             for (let j = 0; j < y; ++j) {
-                if (i === 0 || j === 0 || i === x - 1 || j === y - 1 || (i % 2 === 0 && j % 2 === 0))
+                if (i === 0 || j === 0 || i === x - 1 || j === y - 1 || (i % 2 === 0 && j % 2 === 0)) {
                     map[i][j] = FieldEnum.STONE;
-                else
-                    map[i][j] = FieldEnum.EMPTY;
+                } else {
+                    if (Math.random() < 0.5) {
+                        map[i][j] = FieldEnum.BARRICADE;
+                    } else {
+                        map[i][j] = FieldEnum.EMPTY;
+                    }
+                }
             }
         }
 
         return map;
     }
-
-
 }
--- src/enums/FieldEnum.js
+++ src/enums/FieldEnum.js
@@ -1,4 +1,5 @@
 export const FieldEnum = Object.freeze({
     EMPTY: 0,
-    STONE: 1
+    STONE: 1,
+    BARRICADE: 3,
 });
