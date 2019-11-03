de7a0378f38c9c037e99dc2c14bc49d2744b7653
Alexadnar Karapenev <treildr@gmail.com>
2019-11-03 16:44:21 +0200

    Added more functions, fixed wrong cordinates

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -15,7 +15,10 @@ export class Game extends Phaser.Scene {
 
         this.graphics = this.add.graphics();
         this.gameMap = new GameMap(29, 19);
-        
+
+        this.gameMap.clearForPlayer(1,1);
+        this.gameMap.clearForPlayer(27,17);
+
         for (let i = 0; i < this.gameMap.map.length; i++) {
             for (let j = 0; j < this.gameMap.map[0].length; j++) {
                 if (this.gameMap.map[i][j] === FieldEnum.EMPTY)
@@ -24,7 +27,7 @@ export class Game extends Phaser.Scene {
                     this.graphics.fillStyle(0x808080, 1.0);
                 else
                     this.graphics.fillStyle(0x802b00, 1.0);
-                this.graphics.fillRect(i * 40, j * 40, 40, 40);
+                this.graphics.fillRect(j * 40, i * 40, 40, 40);
             }
         }
     }
