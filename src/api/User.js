a0c3e7feaadadac779b6e270368125c25ee85a33
Alexadnar Karapenev <treildr@gmail.com>
2019-11-07 12:08:18 +0200

    Added movable player

---
+++ src/api/User.js
@@ -0,0 +1,39 @@
+export class User {
+
+    constructor(x, y, size){
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.inTransit = false;
+
+        this.t1 = 0;
+        this.t2 = 0;
+    }
+
+    transit(x, y) {
+        let t = this;
+        if (t.inTransit) return;
+        t.inTransit = true;
+        for (let i = 0; i < 10; ++i) {
+            setTimeout(function() {
+                t.t1 += Math.round((x - t.x) * t.size / 10);
+                t.t2 += Math.round((y - t.y) * t.size / 10);
+            }, 10 * (i + 1));
+        }
+        setTimeout(function() {
+            t.t1 = 0;
+            t.t2 = 0;
+
+            t.x = x;
+            t.y = y;
+
+            t.inTransit = false;
+        }, 100);
+    }
+
+    draw(graphics){
+        graphics.fillStyle(0x802bFF, 1.0);
+        graphics.fillRect(this.x * this.size + this.t1, this.y * this.size + this.t2, 40, 40);
+    }
+
+}
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,8 +1,12 @@
 import {GameMap} from "../api/GameMap.js";
 import {FieldEnum} from "../enums/FieldEnum.js";
+import {User} from "../api/User.js";
 
 let socket = io();
 let gameMap = new GameMap();
+let user = new User(1,1, 40);
+
+let keys = [];
 
 export class Game extends Phaser.Scene {
 
@@ -17,9 +21,15 @@ export class Game extends Phaser.Scene {
     create() {
         this.graphics = this.add.graphics();
         socket.emit('spawn', "");
+
+        keys.push(this.input.keyboard.addKey('A'));
+        keys.push(this.input.keyboard.addKey('S'));
+        keys.push(this.input.keyboard.addKey('D'));
+        keys.push(this.input.keyboard.addKey('W'));
     }
 
     update() {
+        this.graphics.clear();
         for (let i = 0; i < gameMap.map.length; i++) {
             for (let j = 0; j < gameMap.map[0].length; j++) {
                 if (gameMap.map[i][j] === FieldEnum.EMPTY)
@@ -31,6 +41,15 @@ export class Game extends Phaser.Scene {
                 this.graphics.fillRect(j * 40, i * 40, 40, 40);
             }
         }
+        if(keys[0].isDown && gameMap.map[user.y][user.x - 1] !== FieldEnum.STONE)
+            user.transit(user.x - 1, user.y);
+        if(keys[1].isDown && gameMap.map[user.y + 1][user.x] !== FieldEnum.STONE)
+            user.transit(user.x, user.y + 1);
+        if(keys[2].isDown && gameMap.map[user.y][user.x + 1] !== FieldEnum.STONE)
+            user.transit(user.x + 1, user.y);
+        if(keys[3].isDown && gameMap.map[user.y - 1][user.x] !== FieldEnum.STONE)
+            user.transit(user.x, user.y - 1);
+        user.draw(this.graphics);
     }
 
 }
